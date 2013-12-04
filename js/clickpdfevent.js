1cd3fd0092a57ca84fafd3ab86e00ba42052971d
Rosemary Le Faive <lefaive@gmail.com>
2013-12-04 17:50:03 -0500

    Changing collections adds keywords. Also, Dates now synchronize. Started MOOSE keyword autocomplete. Changed some of the management interface.  Set timeout on google analytics events.

--- js/clickpdfevent.js
+++ js/clickpdfevent.js
@@ -3,7 +3,10 @@
     $(document).ready(function () {
       $(".pdfdownload").each(function () {
           $(this).click(function () {
+              var that = this;
               ga('send', 'event', 'PDFs', 'Download ' + $(this).text(), $(this).attr("href"), {'timestamp': new Date(), 'page' : location.href});
+              setTimeout(function () {location.href=that.href;}, 400);
+              return false;
           });
       });
     });
