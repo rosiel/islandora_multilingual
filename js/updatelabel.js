bdf7164512b118390a05f5dafc7ebc5aba53a153
Rosemary Le Faive <lefaive@gmail.com>
2014-01-15 20:36:19 -0800

    halfway refactored

--- js/updatelabel.js
+++
@@ -1,6 +0,0 @@
-function updatelabel() {
-    var label = document.getElementById('edit-label');
-    var title = document.getElementById('edit-titleinfo-title');
-    var subti = document.getElementById('edit-titleinfo-subtitle');
-    label.value = title.value + ' ' + subti.value;
-}
